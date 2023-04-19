419ad3dcb2392b117e6d32be7fc989d30363aab9
FelipeCun <124890827+FelipeCun@users.noreply.github.com>
2023-04-18 21:52:39 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -24,7 +24,7 @@
 //alert(valores.length);
 
 
-let Array
+/*let Array
 console.log('a. ', Array); //a.  undefined
 
 Array = null
@@ -42,3 +42,53 @@ console.log('e. ', Array); //(11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 const valor = Array[i+6];
 console.log('f. ', valor);
 
+const frase = prompt("Digite uma Frase: ");
+console.log(frase.toUpperCase().replaceAll("A", "I"),frase.length);*/
+
+
+//Faça um programa que pergunte ao usuario seu nome e e=mail. Em seguida, imprima no console a seguinte mensagem.
+//O e-mail 'emailDoUsuario' foi cadastrado com sucesso.
+//Seja bem vinda(o), 'nomeDoUsuario'!
+
+/*let nomeDoUsuario = prompt("Qual é o seu nome? ");
+let emailDoUsuario = prompt("Insira seu e-mail? ");
+
+alert("O e-mail " + emailDoUsuario + " foi cadastrado com sucesso." + "\n\nSeja bem vinda(o), " + nomeDoUsuario + "!");*/
+
+
+//Faça um programa, seguindo os passos:    
+//a) Crie um array vazio e guarde-o em uma variável, chamada `listaDeTarefas`  
+//b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+//c) Imprima o array no console
+//d) Peça ao usuário que digite o *índice* de uma tarefa que ele já realizou: 0, 1 ou 2 
+//e) Remova da lista o item de índice que o usuário escolheu.
+//f) Imprima o array no console
+
+/*let listaDeTarefas = [];
+listaDeTarefas = prompt("Cite a primeira tarefa do seu dia: ");
+listaDeTarefas = prompt("Cite a segunda tarefa do seu dia: ");
+listaDeTarefas = prompt("Cite a terceira tarefa do seu dia: ");
+
+console.log(listaDeTarefas);
+
+let indice = prompt("Digite o número 0,1 ou 2 de sua tarefa do dia já realizada: ");
+listaDeTarefas.splice(indice);
+console.log(listaDeTarefas);*/
+
+
+
+//1. Receba uma frase e retorne um array onde cada elemento é uma das palavras da frase, ignorando os espaços
+
+//2. Dado o array `["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]`, faça um programa que acha o índice da palavra Abacaxi e imprime tanto o índice quanto o tamanho do array
+
+//3. Faça um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma variável. Em seguida, siga os passos:
+    
+//a) Imprima no console o array completo  
+//b) Imprima no console a mensagem "Essas são as minhas comidas preferidas: ", seguida por cada uma das comidas, *uma embaixo da outra*.
+//c) Aqui vai um desafio: pergunte ao usuário uma comida preferida. Troque a segunda comida da sua lista pela inserida pelo usuário. Imprima no console a nova lista
+
+//4. Crie um script que pergunte 3 números ao usuário, coloque em um array, depois exiba tal array.
+
+//a)Modifique os elementos do array de modo a a sequência de números ficar do contrário.
+//b)Ou seja, se digitou: 1,2,3
+//c)Vai aparecer: 3,2,1
